ec3623fd706d42ec9b4b00da6635261fd6ac5083
Oyohoukpai <oyohoakpan@gmail.com>
2023-05-21 22:40:50 +0000

    myy task

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application """
-from flask import Flask
-app = Flask(__name__)
+""" A String that Starts a Flash Web Application. Your web application must be listening on 0.0.0.0, port 5000"""
+
+from flask import Flask, render_template
+app = Flask("__name__")
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """ Prints a Message when / is called """
-    return 'Hello HBNB!'
+def hello():
+    """ Return a given string """
+    return render_template("10-hbnb_filters.html")
 
 if __name__ == "__main__":
-    """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=None)
